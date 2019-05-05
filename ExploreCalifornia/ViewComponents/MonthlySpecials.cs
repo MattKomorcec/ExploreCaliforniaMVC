046d6061dc66322c46040ce998f876c7e2fd7dca
Matija <matija.komorcec@gmail.com>
2019-05-05 21:22:01 +0200

    Add project files.

---
+++ ExploreCalifornia/ViewComponents/MonthlySpecials.cs
@@ -0,0 +1,21 @@
+﻿using ExploreCalifornia.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ExploreCalifornia.ViewComponents
+{
+    public class MonthlySpecials : ViewComponent
+    {
+        private readonly BlogDataContext _db;
+
+        public MonthlySpecials(BlogDataContext db)
+        {
+            _db = db;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var specials = _db.MonthlySpecials;
+            return View(specials);
+        }
+    }
+}
